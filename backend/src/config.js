2645017870ee78b4314920083e1b1a488e8507ee
alvaranz83 <103906142+alvaranz83@users.noreply.github.com>
2022-07-03 17:36:14 +0100

    Added Contract address on Config.js
    frontend was crashing

--- backend/src/config.js
+++ backend/src/config.js
@@ -422,7 +422,7 @@ const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the
 const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually set the whitelisted addresses
 
 // ** OPTIONAL **
-let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually include it
+let CONTRACT_ADDRESS = "0x7C7140E90AAE3f624F24cDF16DcFb6b00015c306"; // If you want to manually include it
 
 // Generic Metadata is optional if you want to reveal your NFTs
 const GENERIC = true; // Set to true if you want to upload generic metas and reveal the real NFTs in the future
